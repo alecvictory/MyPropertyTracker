66631a30fdbce5c4a825bc878ec116ec99614bbb
alec victory <alecvictory93@gmail.com>
2021-06-14 16:53:34 -0600

    new application

---
+++ MyPropertyTracker.client/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:5001' : ''
+export const domain = 'dev-xy5n17w4.us.auth0.com'
+export const audience = 'https://MyPropertyTracker'
+export const clientId = 'vmr3g6umRT9ejUECAII3hjVyxVX3nc8p'
