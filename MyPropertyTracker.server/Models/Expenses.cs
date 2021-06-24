4e85a01b592dadddcdeee23616ed9e4c5a0be6a6
alec victory <alecvictory93@gmail.com>
2021-06-23 21:31:55 -0600

    stuff

--- MyPropertyTracker.server/Models/Expenses.cs
+++ MyPropertyTracker.server/Models/Expenses.cs
@@ -1,7 +1,16 @@
 namespace MyPropertyTracker.server.Models
 {
+    // create implement classes to break apart expenses so you can edit the server without having to redo it.
     public class Expenses
     {
-        
+        public int Id { get; set; }
+        public int PropertyTaxes { get; set; }
+        public int Insurance { get; set; }
+        public int Maintenance { get; set; }
+        public int Cleaning { get; set; }
+        public int Repairs { get; set; }
+        public int Supplies { get; set; }
+        public int Utilities { get; set; }
+        public int Miscellaneous { get; set; }
     }
 }
