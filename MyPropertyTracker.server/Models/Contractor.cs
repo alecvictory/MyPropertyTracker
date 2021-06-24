b02782ffaca287de6fe2991fb66cb4693c0c546a
alec victory <alecvictory93@gmail.com>
2021-06-23 22:20:34 -0600

    model changes

--- MyPropertyTracker.server/Models/Contractor.cs
+++ MyPropertyTracker.server/Models/Contractor.cs
@@ -5,6 +5,7 @@ namespace MyPropertyTracker.server.Models
         public int Id { get; set; }
         public string CreatorId { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public string Address { get; set; }
     }
 }
